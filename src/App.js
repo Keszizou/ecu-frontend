f45c3779d65c034a1b1a4141a01052e32b18b9b2
Keszizou <pakozizou@gmail.com>
2025-03-18 08:27:15 +0100

    Frontend ECU

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>ECU Frontend Online</h1>
+      <p>Prueba de despliegue en Render</p>
+    </div>
+  );
+}
+
+export default App;
